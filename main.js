8cc11e2a2b6382d6bfaf92ded01a9bd7392a10a1
YCROBIN72 <97771318+YCROBIN72@users.noreply.github.com>
2022-05-10 20:26:55 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+canvas = 
+document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+ctx.begainPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 1;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
